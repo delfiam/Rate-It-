49638dac9c91bab3cca1a704d92d8ff3b2324f87
Delfi <delfinamunich@gmail.com>
2023-01-12 18:31:10 -0300

    files

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './others/navigation';
+import React from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Main></Main>
   );
 }
 
---
+++ screens/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+export default function HomeScreen () {
+    return (
+        <View>
+        <Text>Hola</Text>
+        </View>
+    );
+};
