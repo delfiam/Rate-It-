1bda28a7fe4ba8c92dcd8fcce1f85ed0d8587aa0
Delfi <delfinamunich@gmail.com>
2023-01-12 23:18:05 -0300

    axios

--- screens/home.js
+++ screens/home.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TextInputBase, TextInput } from "react-native";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function HomeScreen () {
+    const [peliculas, setPeliculas] = useState([]);
+    const [series, setSeries] = useState([]);
+
+    const conseguirPeliculas = async (titulo) => {
+        const response = await axios.get(
+            `http://www.omdbapi.com/?s=${titulo}&apikey=3069e472`)
+        setPeliculas(response.data.Search);
+    }
+    console.log(setPeliculas);
     return (
+
         <View>
-        <Text>Hola</Text>
+        <TextInput  placeholder="Buscar Pelicula" onChangeText={conseguirPeliculas} /> 
         </View>
     );
 };
