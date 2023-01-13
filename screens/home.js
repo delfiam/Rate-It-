5a841db66576f62d2a0852dd50b39d736259a354
Delfi <delfinamunich@gmail.com>
2023-01-13 00:03:37 -0300

    shes going somewhere

--- screens/home.js
+++ screens/home.js
@@ -10,13 +10,25 @@ export default function HomeScreen () {
     const conseguirPeliculas = async (titulo) => {
         const response = await axios.get(
             `http://www.omdbapi.com/?s=${titulo}&apikey=3069e472`)
-        setPeliculas(response.data.Search);
+        response.data.Search.map((pelicula) => {
+            setPeliculas(pelicula.Title);
+        })
     }
     console.log(setPeliculas);
     return (
 
-        <View>
+        <View style={styles.container}>
         <TextInput  placeholder="Buscar Pelicula" onChangeText={conseguirPeliculas} /> 
+        <Text> ${peliculas}</Text>
         </View>
     );
 };
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
